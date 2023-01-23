f37f83fbc7c3ebc03c49dc63893cbc431ae8fbd7
VemComPY <rafabios@users.noreply.github.com>
2023-01-23 15:01:34 -0300

    added dinamic params

--- storage-class.tf
+++ storage-class.tf
@@ -6,6 +6,15 @@ apiVersion: storage.k8s.io/v1
 metadata:
   name: ${var.storage_class_name}
 provisioner: efs.csi.aws.com
+mountOptions:
+  - tls
+parameters:
+  provisioningMode: ${var.efs_provision_mode}
+  fileSystemId: ${var.efs_id}
+  directoryPerms: "700"
+  gidRangeStart: "1000"
+  gidRangeEnd: "2000"
+  basePath: "/dynamic_provisioning"
 YAML
   depends_on = [helm_release.kubernetes_efs_csi_driver]
 }
